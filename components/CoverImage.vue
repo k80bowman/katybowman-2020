a716887155b8ce93f4f547bdd90870b3bd50e4cb
Katy Bowman <katy@heysparkbox.com>
2020-12-09 20:22:36 -0500

    add CoverImage component

---
+++ components/CoverImage.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class='cover-image'>
+    <img v-bind:src='coverImageUrl' />
+    <div v-if=coverImageSourceName class='cover-image__attribution'>
+        <span>source: <a v-bind:href='coverImageSourceUrl'>{{ coverImageSourceName }}</a></span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props:[
+    'coverImageUrl',
+    'coverImageAltText',
+    'coverImageSourceName',
+    'coverImageSourceUrl'
+  ]
+}
+</script>
+
+<style scoped lang='scss'>
+.cover-image {
+  text-align: center;
+  margin: 2rem;
+
+  img {
+    max-width: 100%;
+  }
+
+  &__attribution {
+    font-size: .8rem;
+  }
+}
+</style>
