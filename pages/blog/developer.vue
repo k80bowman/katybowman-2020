a478f340b11934cce63d691ad2c5321924050932
Katy Bowman <katybowman@gmail.com>
2021-01-01 20:21:30 -0500

    add developer blog page

---
+++ pages/blog/developer.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="posts">
+      <h2>Blog Posts - Technical</h2>
+      <PostList :posts="devPosts" />
+    </div>
+</template>
+
+<script>
+import gql from 'graphql-tag';
+import devPostsQuery from '../../queries/devPosts.gql';
+
+export default {
+  name: 'Blog_Developer',
+  data() {
+    return {
+      devPosts: []
+    }
+  },
+  apollo: {
+    devPosts: {
+      query: devPostsQuery,
+    }
+  }
+}
+</script>
+
+<style scoped lang='scss'>
+h2 {
+  font-family: $heading-stack;
+  font-size: 3em;
+  font-weight: normal;
+  margin: 2rem 0 1rem;
+}
+</style>
