15f9bf454090ab94b462d408ceb0a4a440a10d80
Katy Bowman <katybowman@gmail.com>
2021-08-12 08:30:15 -0400

    fix styling of publication cards in post list

--- components/PostList.vue
+++ components/PostList.vue
@@ -1,12 +1,7 @@
 <template>
   <div class="postList">
     <div v-for="post in posts" :key="post.id">
-      <div v-if="post.publication">
-        <PublicationCard :publication="post" />
-      </div>
-      <div v-else>
         <PostCard :post="post" />
-      </div>
     </div>
   </div>
 </template>
